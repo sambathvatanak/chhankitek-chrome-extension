49137836847edc21e535276fcbfdb7e970d741dd
VATANAK SAMBATH <sambathvatanak@gmail.com>
2021-08-11 15:35:38 +0700

    initial

---
+++ script/app.js
@@ -0,0 +1,21 @@
+var moment = momentkh(moment);
+var today = moment();
+document.getElementById("contentArea").innerHTML = today.toKhDate();
+var date = today.format("YYYY-MM-DD");
+$("#pickDate").val(date);
+date = today.toNormalDate(date);
+document.getElementById("contentAreaGeneral").innerHTML = date;
+
+$(document).ready(function(){
+    $("#pickDate").on('change', function () {
+        var date  = $("#pickDate").val();
+        //$("#pickDate").val(date);
+        var newDate = today.toKhDateParameter(undefined, date);           
+        var normalDate = today.toNormalDate(date);
+        document.getElementById("contentArea").innerHTML = newDate; //+ "<br>" + normalDate;
+        document.getElementById("contentAreaGeneral").innerHTML = normalDate       
+    });
+});
+
+
+
